0dbcdc1b9562dcb62ea0a9a2f8617765288479a6
Srishti Ahuja <143893903+Srishti-Ahuja14@users.noreply.github.com>
2025-01-15 14:57:41 +0530

    Added backend of login and signup

---
+++ main_page/firebaseauth.js
@@ -0,0 +1,102 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIMWero_vAVT9tuSWsf3ZfMXVMCkn7oo8",
+  authDomain: "cvfy-20a78.firebaseapp.com",
+  projectId: "cvfy-20a78",
+  storageBucket: "cvfy-20a78.firebasestorage.app",
+  messagingSenderId: "383629739362",
+  appId: "1:383629739362:web:95b2d0e72af76b1e34983b",
+  measurementId: "G-5FZ2GD74DM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function showMessage(message, divId){
+    var messageDiv = document.getElementById(divId);
+    messageDiv.style.display = "block";
+    messageDiv.innerHTML = message;
+    messageDiv.style.opacity = 1;
+    setTimeout(function(){
+        messageDiv.style.opacity = 0;
+    }, 5000)
+}
+
+const signUp = document.getElementById("signUp");
+signUp.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email = document.getElementById('remail').value;
+    const password = document.getElementById('rpassword').value;
+    const name = document.getElementById('rname').value;
+
+    const auth = getAuth();
+    const db = getFirestore();
+    
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential)=>{
+        const user = userCredential.user;
+        const userData = {
+            email: email,
+            name: name,
+        }
+        showMessage("Success!!", 'signUpMessage')
+        const docRef = doc(db, "users", user.uid);
+        setDoc(docRef, userData).then(()=>{
+            window.location.href = 'index.html'
+        })
+        .catch((error)=>{
+            console.error("error writing document", error)
+        })
+
+    })
+    .catch((error)=>{
+        const errorCode = error.code;
+        if(errorCode=='auth/email-already-in-use'){
+            showMessage('Email Address Already Exists!', 'signUpMessage')
+        }
+        else if(errorCode == 'auth/invalid-email'){
+            showMessage('Invalid Email Address!', 'signUpMessage')
+        }
+        else if(errorCode == 'auth/weak-password'){
+            showMessage('Weak Password! Must be at least 6 characters', 'signUpMessage')
+        }
+        else{
+            showMessage('Unable to create user', 'signUpMessage')
+        }
+    })
+})
+
+
+const signIn = document.getElementById('signIn')
+signIn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const auth = getAuth()
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+        showMessage('Login Successful', 'signInMessage')
+        const user = userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid)
+        window.location.href = 'index.html';
+    })
+    .catch((error)=>{
+        const errorCode = error.code;
+        if(errorCode == 'auth/invalid-credential'){
+            showMessage('Incorrect Email or Password', 'signInMessage')
+        }
+        else{
+            showMessage('Account does not exist', 'signInMessage')
+        }
+    })
+})
